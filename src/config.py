e52075710e844812e52b4907a47e92a05e9669f7
Wyatt Shapiro <wyattshapiro@gmail.com>
2019-06-16 18:17:25 -0400

    add config file to specify variables used to connect to db

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# Note: Storing crendentials is not recommended.
+# It is better practice to set as an environmental variable and dynamically retreive
+# For purpose of demo code, will use this config.py as a mock .bash_profile
+
+# Database connection environment variables
+DB_HOST = '127.0.0.1'
+DB_PORT = '5432'
+DB_NAME_DEFAULT = 'studentdb'  # default database for user
+DB_NAME_SPARKIFY = 'sparkifydb'  # new database to store app data
+DB_USER = 'wyattshapiro'
+DB_PASSWORD = ''
